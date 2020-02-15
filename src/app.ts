756fdfab63e5d65ad05af5b6398e51279d82aee3
cvdat2097 <cvdat2097@gmail.com>
2020-02-15 13:01:05 +0700

    add eslint

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,12 @@ const app = express();
 console.log('Running in', process.env.NODE_ENV);
 
 app.use('/', (req, res) => {
-  res.send(`Hi, I'm thunder-core!`);
+  res.send('Hi, Im thunder-core!');
+  const x = 3;
+  switch (x) {
+    case 3:
+      return 2;
+  }
 });
-
-const x = 'skldjflsdf';
 
 app.listen(3000);
