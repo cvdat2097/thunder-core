8a7b3035c3d0b81e420ce309e5a5b6a4a4ec9e95
cvdat2097 <cvdat2097@gmail.com>
2020-02-15 11:26:15 +0700

    initial skeleton

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+
+console.log('Running in', process.env.NODE_ENV);
+
+app.use('/', (req, res) => {
+  res.send(`Hi, I'm thunder-core!`);
+});
+
+app.listen(3000);
